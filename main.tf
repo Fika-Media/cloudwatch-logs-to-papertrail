4ef5d0c61a4c28fe0b76b13dbb144ced77b7da9b
ca77y <ca77y@users.noreply.github.com>
2023-05-23 17:44:57 +0200

    main files

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+module "cloudwatch-log-group-x-papertrail" {
+  source                  = "./tf_module_0.12"
+  monitor_log_group_names = []
+  papertrail_host         = "logs.papertrailapp.com"
+  papertrail_port         = "48745"
+  filter_pattern          = ""
+  timeout                 = "10"
+  lambda_log_role_arn     = "arn:aws:iam::105642504987:role/Laterless-papertrail"
+  lambda_name_prefix      = "laterless"
+  parse_log_levels        = "false"
+}
